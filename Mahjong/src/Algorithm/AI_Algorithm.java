b37e97eda99679eb88a13f7a4aa9f1ebf9cae744
ZhangBohan <bohan.zhang@ucdconnect.ie>
2024-05-17 16:11:20 +0800

    Create new method EasyAI in AI_Algorithm class

--- Mahjong/src/Algorithm/AI_Algorithm.java
+++ Mahjong/src/Algorithm/AI_Algorithm.java
@@ -9,4 +9,21 @@ import java.util.ArrayList;
 import java.util.TreeSet;
 
 public class AI_Algorithm {
+    GameJFrame g;
+    public void EasyAI(int playerIndex,MahjongCard current){
+        ArrayList<MahjongCard> player= g.playerList.get(playerIndex);
+        if(Hu_Algorithm.checkHu(player)){
+            PlayerOperation.PengCards(playerIndex);
+        }
+        else if(Other_Algorithm.CheckGang(player,current)){
+            PlayerOperation.PengCards(playerIndex);
+        }
+        else if(Other_Algorithm.CheckPeng(player,current)){
+            PlayerOperation.PengCards(playerIndex);
+        }
+        else if(Other_Algorithm.CheckChi(player,current)){
+            PlayerOperation.PengCards(playerIndex);
+        }
+        else{ PlayerOperation.ShowCard(playerIndex);}
+    }
 }
